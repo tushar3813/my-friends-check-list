8a43b5a05d3c7efc0cd99116a36903b8445f3ff4
tushar3813 <48579284+tushar3813@users.noreply.github.com>
2026-06-11 14:22:37 +0530

    Add files via upload

---
+++ mst.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class mst {
+    public static void main(String[] args) {
+
+        String[] myfriends = {
+                "sneha",
+                "dakash",
+                "simran",
+                "nishu",
+                "vishakha",
+                "priya"
+        };
+
+        System.out.println("Enter your name:");
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+
+        boolean check = false;
+
+        for (String elements : myfriends) {
+            if (name.equals(elements)) {
+                check = true;
+                break;
+            }
+        }
+
+        if (check) {
+            System.out.println("Ap Tushar ke dost ho!");
+        } else {
+            System.out.println("Ap Tushar ke dost nahi ho!");
+        }
+    }
+}
